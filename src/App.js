89ceaa4eb7ea7dc79f54ff862698627e67702ddf
DavidLiu <dvdgege5@gmail.com>
2018-03-04 17:51:26 -0800

    add flexbox styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './img/activity.svg';
 import './App.css';
+import './css/responsive.css';
 
 class App extends Component {
 	render() {
@@ -8,7 +9,7 @@ class App extends Component {
 			<div className="App">
 
 				<div id="app-header" className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
+					<img src={logo} className="App-logo svgLogo" alt="logo" />
 					<h1>Circadian</h1>
 					<a className="button" href="#APP-BODY">Start</a>
 				</div>
@@ -25,11 +26,28 @@ class Body extends Component {
 	render() {
 		return (
 			<div id="APP-BODY">
-				<h2>Life in Numbers</h2>
-				<p>Journal your daily life in numbers, and review whenever.</p>
+				<Intro />
 			</div>
 		);
 	}
 }
+
+
+class Intro extends Component {
+	render() {
+		return(
+			<div>
+				<h2>Life in Numbers</h2>
+				<div className="flex-container">
+					<p className="flex-1-2">Journal your daily life in numbers, and review whenever.</p>
+					<div className="flex-1-2">
+						lul
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
 
 export default App;
