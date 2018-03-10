27c0e08f8e2a65d17f416ce570c9786638f3d878
DavidLiu <dvdgege5@gmail.com>
2018-03-10 14:29:47 -0800

    add app.run() to main so server can be launched with python server.py

--- py_server/server.py
+++ py_server/server.py
@@ -1,7 +1,7 @@
 # Circadian API server
 
-from flask import Flask
-from flask import abort, errorhandler, session, request
+from flask import Flask, Response, redirect, url_for, request, session, abort
+#from flask import abort, session, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,3 +13,7 @@ def fake_login():
 	if request.method == 'POST':
 		pass
 		
+
+# launch server with: python server.py
+if __name__ == "__main__":
+    app.run()
