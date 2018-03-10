a1c006f3d4cdc3205a4363ef44155bbc027f7612
DavidLiu <dvdgege5@gmail.com>
2018-03-10 13:15:20 -0800

    add some basic logic to server

--- py_server/server.py
+++ py_server/server.py
@@ -1,1 +1,15 @@
 # Circadian API server
+
+from flask import Flask
+from flask import abort, errorhandler, session, request
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+	return 'Hello, World!'
+
+@app.route( '/login', methods=['GET', 'POST'] )
+def fake_login():
+	if request.method == 'POST':
+		pass
+		
