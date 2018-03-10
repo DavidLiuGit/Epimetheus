c9ad11a28553041d99833757dc766a0e6a19e11d
DavidLiu <dvdgege5@gmail.com>
2018-03-10 15:32:38 -0800

    add flask-cors; make sure to pip install flask-cors --user before launching server

--- py_server/server.py
+++ py_server/server.py
@@ -1,18 +1,29 @@
 # Circadian API server
 
 from flask import Flask, Response, redirect, url_for, request, session, abort
-#from flask import abort, session, request
+from flask_cors import CORS
+import users
+from json import dumps
+
+# app config
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/')
 def hello_world():
 	return 'Hello, World!'
+
 
 @app.route( '/login', methods=['GET', 'POST'] )
 def fake_login():
 	if request.method == 'POST':
 		pass
 		
+
+@app.route ( '/mystream', methods=['GET'] )
+def stream_Data():
+	return dumps ( users.user )
 
 # launch server with: python server.py
 if __name__ == "__main__":
